f5e011091b769db24dc63dcefabb54a10afb5c6e
Carlos Zabala <czabala847@gmail.com>
2024-05-20 11:00:00 -0500

    config db with sequalize

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import colors from "colors";
 import server from "./server";
 
-server.listen(4000, () => {
-    console.log("server running on port 4000");
-})
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(colors.bgCyan.white.bold(`server running on port ${port}`));
+});
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
+import colors from "colors";
 import express from "express";
+import db from "./config/db";
 import router from "./router";
 
+async function connectDB() {
+  try {
+    await db.authenticate();
+    await db.sync();
+    console.log(colors.bgGreen.white.bold("DB conectada"));
+  } catch (error) {
+    console.error(colors.bgRed.white.bold("Error conectar DB"), error);
+  }
+}
+
+connectDB();
 const server = express();
 
 server.use("/api/products", router);
