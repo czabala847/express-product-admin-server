aed2c0aec11fd5ee2ac2f1decc5e166074e84f54
Carlos Zabala <czabala847@gmail.com>
2024-05-22 11:36:39 -0500

    finish api products

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,9 +1,106 @@
 import { Request, Response } from "express";
+// import { check, validationResult } from "express-validator";
 import Product from "../models/Product.module";
 
+export const getProductsById = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findByPk(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json({ data: product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.findAll({
+      order: [["price", "ASC"]],
+    });
+    res.json({ data: products });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 export const createProduct = async (req: Request, res: Response) => {
-  //   const product = new Product(req.body);
-  //   const savedProduct = await product.save();
-  const product = await Product.create(req.body);
-  res.json({ data: product });
+  //Validación en el controlador -> se mueve a la ruta
+  // await check("name").notEmpty().withMessage("Name is required").run(req);
+  // await check("price")
+  //   .isNumeric()
+  //   .withMessage("Price must be numeric")
+  //   .notEmpty()
+  //   .withMessage("Price is required")
+  //   .custom((value) => value > 0)
+  //   .withMessage("Price must be greater than 0")
+  //   .run(req);
+
+  //Validación en el controlador -> se mueve a un middleware
+  // let errors = validationResult(req);
+
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).json({ errors: errors.array() });
+  // }
+
+  try {
+    const product = await Product.create(req.body);
+    res.json({ data: product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    await product.update(req.body);
+    await product.save();
+    res.json({ data: product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProductAvailability = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    product.availability = !product.dataValues.availability;
+    await product.save();
+
+    res.json({ data: product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    await product.destroy();
+    res.json({ message: "Product deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,68 @@
 import { Router } from "express";
-import { createProduct } from "./handlers/product";
+import { body, param } from "express-validator";
+import {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  getProductsById,
+  updateProduct,
+  updateProductAvailability,
+} from "./handlers/product";
+import { handleInputErrors } from "./middleware";
 const router = Router();
 
 //Routing
-router.get("/", (req, res) => {
-  res.json("Desde GET");
-});
+router.get("/", getProducts);
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("Invalid ID"),
+  handleInputErrors,
+  getProductsById
+);
 
-router.post("/", createProduct);
+router.post(
+  "/",
+  // validacion en la ruta
+  body("name").notEmpty().withMessage("Name is required"),
+  body("price")
+    .isNumeric()
+    .withMessage("Price must be numeric")
+    .notEmpty()
+    .withMessage("Price is required")
+    .custom((value) => value > 0)
+    .withMessage("Price must be greater than 0"),
+  handleInputErrors,
+  createProduct
+);
 
-router.put("/", (req, res) => {
-  res.json("Desde PUT");
-});
+router.put(
+  "/:id",
+  param("id").isInt().withMessage("Invalid ID"),
+  body("name").notEmpty().withMessage("Name is required"),
+  body("price")
+    .isNumeric()
+    .withMessage("Price must be numeric")
+    .notEmpty()
+    .withMessage("Price is required")
+    .custom((value) => value > 0)
+    .withMessage("Price must be greater than 0"),
+  body("availability").isBoolean().withMessage("Availability must be boolean"),
+  handleInputErrors,
+  updateProduct
+);
 
-router.delete("/", (req, res) => {
-  res.json("Desde DELETE");
-});
+router.patch(
+  "/:id",
+  param("id").isInt().withMessage("Invalid ID"),
+  handleInputErrors,
+  updateProductAvailability
+);
+
+router.delete(
+  "/:id",
+  param("id").isInt().withMessage("Invalid ID"),
+  handleInputErrors,
+  deleteProduct
+);
 
 export default router;
