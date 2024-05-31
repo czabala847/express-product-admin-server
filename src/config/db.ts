dfbf4523b9ddd39c140ba99d61a0b5cff83176d1
Carlos Zabala <czabala847@gmail.com>
2024-05-31 09:23:06 -0500

    config build prod

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
-import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv"
+import { Sequelize } from "sequelize-typescript"
 
 dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-  models: [__dirname + "/../models/**/*.ts"],
+  models: [__dirname + "/../models/**/*"],
   logging: false,
 });
 
